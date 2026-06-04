0835b7fb23e3124d9867d3abd0c90423f2ff5cff
Ярослав <you@example.com>
2026-06-04 23:57:59 +0300

    Add Algorythms/, Services/ and Models/, Views/, ViewModels/ forbetter separation of concerns(UI, logic and bindings). Add RelayCommand.cs and BaseViewModel.cs to Services/ for base WPF MVVM implementation

---
+++ Services/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<Type>(ref Type storage, Type value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TrafficSim
+namespace TrafficSim.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
